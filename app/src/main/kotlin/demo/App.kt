63343f2a7aa9f2aef7493a520f94df892357c379
Schielek, Richard <56879896+schielek@users.noreply.github.com>
2023-08-16 19:39:24 +0200

    Add ThreadLocal to example to not only focus on MDC

--- app/src/main/kotlin/demo/App.kt
+++ app/src/main/kotlin/demo/App.kt
@@ -6,35 +6,35 @@ import io.vertx.ext.web.client.WebClient
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.await
 import io.vertx.kotlin.coroutines.dispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlinx.coroutines.slf4j.MDCContext
-import kotlinx.coroutines.withContext
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 
+val requestContext: ThreadLocal<String?> = ThreadLocal()
 
 class AppVerticle : CoroutineVerticle() {
     override suspend fun start() {
         val router = Router.router(vertx).apply {
             route().handler { ctx ->
                 logger.info("This handler will set some thread local value")
-                MDC.put("context", ctx.request().headers().get("context"))
+                val context = ctx.request().headers().get("context")
+                requestContext.set(context)
+                MDC.put("context", context)
                 logger.info("But how can it pass it along to subsequent handlers as Coroutine-ContextElement?")
                 ctx.next()
             }
 
             get("/demo").handler { ctx ->
                 launch(vertx.dispatcher()) {
-                    logger.info("Same thread, MDC preserved")
+                    logger.info("Same thread, MDC and threadLocal preserved: ${requestContext.get()}")
 
-                    withContext(Dispatchers.Default + MDCContext()) {
-                        logger.info("Thread changed, MDC preserved, but this handler must not know that the MDC context was set by a previous handler")
+                    withContext(Dispatchers.Default + MDCContext() + requestContext.asContextElement()) {
+                        logger.info("Thread changed, MDC and thread-local ${requestContext.get()} preserved, but this handler must not know that the MDC context was set by a previous handler")
                     }
 
                     val response: String? = withContext(Dispatchers.Default) {
-                        logger.info("Thread changed, MDC lost")
+                        logger.info("Thread changed, MDC lost, thread-local lost: ${requestContext.get()}")
                         MDC.get("context")
                     }
 
